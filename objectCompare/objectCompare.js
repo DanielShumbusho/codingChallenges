655727c29d2bfeb788f0524ccb0d298d5dc4312a
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-05-05 13:30:59 +0200

    object compare debug completed

--- objectCompare/objectCompare.js
+++ objectCompare/objectCompare.js
@@ -4,7 +4,7 @@ function deepCompare(obj1, obj2) {
   
     if (
       typeof obj1 !== 'object' || obj1 === null ||
-      typeof obj2 !== 'object' || obj === null
+      typeof obj2 !== 'object' || obj2 === null
     ) {
       return false;
     }
@@ -16,10 +16,12 @@ function deepCompare(obj1, obj2) {
         return false;
   
     for (let key of keys1) {
-      if (!keys2.includes(key) || deepCompare(obj1[key], obj2[key])) {
+      if (!keys2.includes(key) || !deepCompare(obj1[key], obj2[key])) {
         return false;
       }
     }
+  
+    return true;
  }
  console.log(deepCompare({ a: 1 }, { a: 1 })); // true
  console.log(deepCompare({ a: 1 }, { a: 2 })); // false
